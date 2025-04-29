b77c2786b49d98087ef2003bd7e3a540a4c8d293
Abhishek <abhishekjalkhare2003@gmail.com>
2025-04-29 15:32:29 +0530

    login options added

--- app.js
+++ app.js
@@ -9,6 +9,35 @@ const usermodel = require("./models/user.model.js");
 const chatModel = require("./models/chat.model.js"); // Import Chat model
 const { log } = require("console");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { cp } = require("fs");
+app.use(cookieParser());
+const bcrypt = require("bcrypt");
+
+
+const authMiddleware = async (req, res, next) => {
+  const jwtToken = req.cookies.jwt;
+  //console.log(jwt);
+  if (!jwtToken) {
+     res.redirect("/auth/google");
+  }
+  const decoded =  jwt.verify(jwtToken, process.env.JWT_SECRET);
+  if (!decoded) {
+    res.redirect("/auth/google");
+  }
+  else {
+    const user = await usermodel.findById(decoded.id);
+    if (!user) {
+      res.redirect("/auth/google");
+    } else {
+      req.user = user;
+      next();
+    }
+   
+  }
+  
+}  
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -47,7 +76,46 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get("/", (req, res) => {
-  res.send('<h1>Home</h1><a href="/auth/google">Login with Google</a>');
+  res.render('homepage');
+});
+
+app.get('/login' , (req, res) => {
+  res.render('login')
+});
+
+app.post('/login' , async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usermodel.findOne({ email });
+  
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const id = user._id;
+  const token = await jwt.sign({ id }, process.env.JWT_SECRET, );
+  
+  res.cookie("jwt", token, { httpOnly: true });
+  res.redirect("/profile");
+});
+
+app.get('/signup' , (req, res) => { 
+  res.render('signup');
+});
+
+app.post('/signup' , async (req, res) => {
+  const { email, password, displayName , imageUrl } = req.body;
+
+  const user = await usermodel.create({
+    email, password, displayName 
+  });
+  if(imageUrl) {
+    user.image = imageUrl;
+  }
+  await user.save();
+  res.redirect('/login');
 });
 
 app.get(
@@ -70,17 +138,23 @@ app.get(
         displayName,
         image,
       });
-      const saveduser = await newuser.save();
+      const user = await newuser.save();
+      
     }
+    const id = user._id;
+    const token = await jwt.sign({ id }, process.env.JWT_SECRET, );
+    
+    res.cookie("jwt", token, { httpOnly: true });
     res.redirect("/profile");
   }
 );
 
-app.get("/profile", async (req, res) => {
-  console.log(req.user);
+app.get("/profile", authMiddleware , async (req, res) => {
 
+ 
   const LoggedInUser = await usermodel.findOne({
-    email: req.user.emails[0].value,
+    
+    email: req.user.email,
   });
 
   const onlineUser = await usermodel.find({
@@ -92,24 +166,22 @@ app.get("/profile", async (req, res) => {
     },
   });
 
-  console.log(onlineUser);
-
   res.render("chat", { user: LoggedInUser, onlineUser });
 });
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 io.on("connection", (socket) => {
-  console.log("user connected");
-  console.log(socket.id);
+  
+  
 
   socket.on("join", async (id) => {
-    console.log("connect");
+    
     await usermodel.findByIdAndUpdate(id, { socketId: socket.id });
   });
 
   socket.on("message", async (obj) => {
-    console.log(obj);
+    
 
     // Validate the message content
     if (!obj.message || obj.message.trim() === "") {
@@ -161,10 +233,9 @@ io.on("connection", (socket) => {
 });
 
 // Endpoint to fetch chat history
-app.get("/chats/:receiverId", async (req, res) => {
+app.get("/chats/:receiverId",authMiddleware , async (req, res) => {
   const senderId = req.user._id; // Assuming user is authenticated
   const receiverId = req.params.receiverId;
-
   const chats = await chatModel
     .find({
       $or: [
@@ -175,6 +246,12 @@ app.get("/chats/:receiverId", async (req, res) => {
     .sort({ timestamp: 1 });
 
   res.json(chats);
+  
+  
 });
+
+
+
+
 
 server.listen(3000);
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     googleId: {
         type: String,
@@ -15,16 +16,29 @@ const userSchema = new mongoose.Schema({
     },
     image: {
         type: String,
+        default:'./resources/default-profile-picture.avif'
     },
     socketId: {
         type: String,
         default: ''
-    }
+    },
+    password: {
+        type: String,
+       
+    },
 },
     {
         timestamps: true
     }
 );
+
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 const User = mongoose.model("user" , userSchema);
 module.exports = User;
